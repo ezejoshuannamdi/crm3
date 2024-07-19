0f8b68af689d4849b9dcece6b0630fa97ac8ea8b
ezejoshuannamdi <zagadatmali@gmail.com>
2024-07-19 14:59:23 +0100

    updated allowed hosts

--- hotel_management/settings.py
+++ hotel_management/settings.py
@@ -32,9 +32,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "false")
+DEBUG = os.getenv("DEBUG", False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["crm3-ajmo.onrender.com", "127.0.0.1"]
 
 
 # Application definition
